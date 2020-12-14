353a7efadf1166c91c36cee3e6bdae4d64e2e799
freesummerwind <liz-e-tta@yandex.ru>
2020-12-15 01:46:19 +0300

    add neuro_notes_duration code and demo for it

---
+++ demo_neuro_notes_length.py
@@ -0,0 +1,14 @@
+from tensorflow.keras.models import load_model
+from neuro_notes_length import notes_from_image
+from neuro_notes_length import note_durations
+import numpy as np
+
+
+if __name__ == '__main__':
+    model = load_model('notes_length.h5')
+
+    notes = notes_from_image('datasets/made_from_primus_dataset/train/000051763-1_1_1/000051763-1_1_1.png')  # path to picture with notes
+    prediction = model.predict(notes)
+
+    for i in prediction:
+        print(note_durations[np.where(i == 1)[0][0]])
